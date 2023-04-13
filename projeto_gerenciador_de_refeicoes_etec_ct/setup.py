b259c6419a63aa7391d4077850db29a19cf1975c
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2023-04-13 16:20:23 -0300

    Update setup.py

--- projeto_gerenciador_de_refeicoes_etec_ct/setup.py
+++ projeto_gerenciador_de_refeicoes_etec_ct/setup.py
@@ -1,12 +1,15 @@
 import cx_Freeze
+import sys
 
-executables = [cx_Freeze.Executable('main.py', base = "Win32GUI")]
+base = "Win32GUI" if sys.platform == "win32" else None
+
+executables = [cx_Freeze.Executable('main.py', base = base)]
 
 cx_Freeze.setup(
     name="Sistema de Controle da Etec",
     options={'build_exe':
                  {
-                    'packages': ['tkinter', 'customtkinter', 'datetime', 'hashlib', 'os', 'pyqrcode', 'threading', 'imghdr', 'smtplib', 'email'],
+                    'packages': ['tkinter', 'customtkinter', 'datetime', 'hashlib', 'os', 'pyqrcode', 'threading', 'imghdr', 'smtplib', 'email', 'pyzbar'],
                     'include_files': [
                         'assets'
                     ]
